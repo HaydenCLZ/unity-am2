6004736ddd985fb4ee0903b85deda84662f94e35
Hayden <ilyes.elkhedim@etu.univ-lyon1.fr>
2023-09-23 15:47:58 +0200

    Spider moving to target

--- Tuto spider/Assets/LegStepper.cs
+++ Tuto spider/Assets/LegStepper.cs
@@ -11,42 +11,19 @@ public class LegStepper : MonoBehaviour
 
     public Transform target;
 
-    public float moveDuration;
-    float timeElapsed = 0;
-    bool moving = false;
+    public float speed = 3f;
 
-    IEnumerator MoveToHome()
-    {
-        moving = true;
-        Vector3 startPoint = transform.position;
-        Vector3 endPoint = target.position;
-        do
-        {
-            // Add time since last frame to the time elapsed
-            timeElapsed += Time.deltaTime;
-
-            float normalizedTime = timeElapsed / moveDuration;
-            transform.position = Vector3.Lerp(startPoint, endPoint, normalizedTime);
-
-            // Wait for one frame
-            yield return null;
-        }
-        while (timeElapsed < moveDuration);
-        moving = false;
-    }
 
     void Update()
     {
-        // If we are already moving, don't start another move
-        if (moving) return;
-
         float distFromHome = Vector3.Distance(transform.position, target.position);
-
-        // If we are too far off in position or rotation
-        if (distFromHome > maxDistance)
+        Vector3 startPoint = transform.position;
+        Vector3 endPoint = target.position;
+        // If we are too far off in position
+        if (distFromHome > maxDistance && distFromHome > minDistance)
         {
-            // Start the step coroutine
-            StartCoroutine(MoveToHome());
+            Vector3 newpos = Vector3.MoveTowards(startPoint, endPoint, speed * Time.deltaTime);
+            transform.position = new Vector3(newpos.x, startPoint.y, newpos.z);
         }
     }
 }
