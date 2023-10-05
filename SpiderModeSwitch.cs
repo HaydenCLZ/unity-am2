858f4f9ef6ea4a84209bc245e37a42756bdace95
Sasha D <97671299+Sashadjm@users.noreply.github.com>
2023-10-05 11:10:40 +0200

    essai implémentation changement entre mode NPC et manual control

---
+++ SpiderModeSwitch.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * A simple class which allows switching between a player input spider controller 
+ * and a spider randomly controlled.
+ * A switch of controller and camera is performed.
+ * We need to make sure two cameras for both mode is settled.
+ * */
+
+public class SpiderModeSwitch : MonoBehaviour {
+
+    public SpiderController spiderController;
+    public SpiderNPCController spiderNPC;
+    public Camera controllerCam;
+    public Camera npcCam;
+
+    void Start() {
+        //Start with spider camera enabled
+        if (controllerCam.enabled && npcCam.enabled) npcCam.enabled = false;
+        if (!controllerCam.enabled && !npcCam.enabled) controllerCam.enabled = true;
+
+        // Start with spider controlled by us enabled
+        if (spiderController.enabled && spiderNPC.enabled) spiderNPC.enabled = false;
+        if (!spiderController.enabled && !spiderNPC.enabled) spiderController.enabled = true;
+    }
+    void Update() { //if we decide to press tab an d change the way the spider is working.
+
+        if (Input.GetKeyDown(KeyCode.Tab)) {
+            controllerCam.enabled = !controllerCam.enabled;
+            npcCam.enabled = !npcCam.enabled;
+            spiderNPC.enabled = !spiderNPC.enabled;
+            spiderController.enabled = !spiderController.enabled;
+        }
+    }
+}
