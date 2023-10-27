278567f384c7075ff7f6fb3f2c1d822c6c5c41e9
Ilyes <haydenelkhedim@gmail.com>
2023-10-27 14:40:48 +0200

    Spider in main scene

---
+++ AHaChaSha/Assets/Assets Alexis/Scripts/CapsuleControler.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CapsuleControler : MonoBehaviour
+{
+    public float speed = 30.0f;
+    public float rotationSpeed = 5.0f;
+    int collide = 0;
+    bool moving = false;
+    bool jumping = false;
+    Vector3 inertie = Vector3.zero;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        collide++;
+    }
+
+    // Gets called when the object exits the collision
+    void OnCollisionExit(Collision collision)
+    {
+        collide--;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {   
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 side = transform.TransformDirection(Vector3.right);
+        inertie *= (0.95f - Time.deltaTime) * (0.9f-Mathf.Min(0.1f, collide));
+        Vector3 movement = Vector3.zero;
+        moving = false;
+        jumping = false;
+        Vector2 mouseInput = new Vector2(Input.GetAxis("Mouse X") * rotationSpeed, 0);
+        Vector2 keyInput = Vector2.zero;
+        if (collide != 0)
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                keyInput.x++;
+                moving = true;
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                keyInput.x--;
+                moving = true;
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                keyInput.y++;
+                moving = true;
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                keyInput.y--;
+                moving = true;
+            }
+            if (Input.GetKey(KeyCode.Space))
+            {
+                jumping = true;
+                moving = true;
+            }
+        }
+        forward *= speed * Time.deltaTime * keyInput.x * -1;
+        side *= speed * Time.deltaTime * keyInput.y * -1;
+        movement = forward + side;  
+        if(jumping)
+        {
+            movement = movement + Vector3.up;
+        }
+        if (moving)
+        {
+            inertie = movement;
+        }
+        else
+        {
+            movement = inertie;
+        }
+        transform.position += movement;
+        transform.Rotate(Vector3.up, mouseInput.x * rotationSpeed);
+    }
+}
