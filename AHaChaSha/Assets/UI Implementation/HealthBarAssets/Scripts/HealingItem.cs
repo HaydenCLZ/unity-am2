e07a01c02286f74d05c6b2f9cba5d48ce2514386
Ilyes <haydenelkhedim@gmail.com>
2023-11-09 10:28:44 +0100

    Heatlh + Menu

---
+++ AHaChaSha/Assets/UI Implementation/HealthBarAssets/Scripts/HealingItem.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class HealingItem : MonoBehaviour
+{
+    public int bonusHealth = 20; 
+    private Player player;
+
+    void Start()
+    {
+        // find the Player script in the scene
+        player = FindObjectOfType<Player>();
+        if (player == null)
+        {
+            Debug.LogError("no player :("); //debugging
+        }
+    }
+
+    // OnTriggerEnter is called when a trigger collider enters the item's collider
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && player != null) //make sure to put properly the tag "player" to the character we use.
+        {
+            if (player.currentHealth < player.maxHealth)
+            {
+                Debug.Log("Player is colliding with the item."); // debugging
+                Destroy(gameObject); // destroy the healing item
+                player.Heal(bonusHealth);
+                Debug.Log("Player's health increased to: " + player.currentHealth); //debugging
+            }
+        }
+    }
+}
