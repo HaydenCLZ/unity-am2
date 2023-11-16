58160a01fb980eb1cad689d9782d0ce6717f62f7
Cha <charlotte.hairon@etu.univ-lyon1.fr>
2023-11-16 08:34:34 +0100

    Assets Cha

---
+++ AHaChaSha/Assets/Assets_Cha/AnimationPerso.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationPerso : MonoBehaviour
+{
+
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InputsVerification();
+    }
+
+    private void InputsVerification() //Vérifie si le perso marche ou coure
+    {
+        //Bool marche 
+        bool marche = animator.GetBool("marche");
+
+        //Bool court
+        bool court = animator.GetBool("court");
+
+        //Stock un bool d'appuie de touche
+        bool forwardPress = Input.GetKey("w");
+        bool leftPress = Input.GetKey("a");
+        bool rightPress = Input.GetKey("d");
+        bool backwardPress = Input.GetKey("s");
+
+        bool runPress = Input.GetKey("left shift");
+
+        //Vérifie qu'au moins une touche est appuyée 
+        if (!marche && (forwardPress || leftPress || rightPress || backwardPress))
+        {
+            animator.SetBool("marche", true);
+           
+        }
+
+        //Vérifie qu'aucune touche n'est appuyée
+        if (marche && !forwardPress && !leftPress && !rightPress && !backwardPress)
+        {
+            animator.SetBool("marche", false);
+        }
+
+        //Vérifie si le perso court
+        if (!court && marche && runPress)
+        {
+            animator.SetBool("court", true);
+        }
+
+        if (court && (!marche || !runPress))
+        {
+            animator.SetBool("court", false);
+        }
+
+    }
+}
+
---
+++ AHaChaSha/Assets/Assets_Cha/MouvementPerso.cs
@@ -0,0 +1,109 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouvementPerso : MonoBehaviour
+{
+    //Vitesse du joueur
+
+    [Header("Movement")]
+    public float moveSpeed;
+
+    //Variables de verification que le joueur est sur le sol
+
+    [Header("Ground Check")]
+    public float groundDrag;
+    /* public float playerHeight;
+    public LayerMask whatIsGround;
+    bool grounded;
+    private float distToGround; */
+
+
+    //Variable d'inputs et d'orientation
+    public Transform orientation;
+    float horizontalInput;
+    float verticalInput;
+
+    Vector3 moveDirection;
+
+    Rigidbody rb;
+
+
+    private void Start()
+    {
+        //Génération du rigidbody
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+
+        //Calcule la distance entre le centre du perso et le sol.
+        //distToGround = GetComponent<Collider>().bounds.extents.y; ;
+
+
+    }
+
+    private void Update()
+    {
+        //GroundCheck();
+        Inputs();
+        SpeedControl();
+        rb.drag = groundDrag;
+        /*if (grounded)
+            rb.drag = groundDrag;
+        else
+            rb.drag = 0;*/
+
+    }
+        private void FixedUpdate()
+    {
+        MouvPerso();
+    }
+  
+    /*private void GroundCheck()
+    {
+        grounded = Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.2f);
+    }*/
+
+    private void Inputs() //R�cup�re les touches directionnelles 
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+    }
+        
+    
+    private void MouvPerso()
+    {
+        //Calcule la direction du mouvement
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        //Donne une impulsion de mouvement
+        rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+    }
+
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+
+       // Limite la vitesse
+       if(flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }
+    }
+
+    /* private void GroundCheck()
+    {
+        //Verifie si le raycast atteint le sol
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+    }
+*/
+    /* private void Drag()
+    {
+        if (grounded)
+            rb.drag = groundDrag;
+        else
+            rb.drag = 0;
+    }
+    */
+}
