904355b8dc5aab4b32be89a392d5919606b93fc2
Sasha D <97671299+Sashadjm@users.noreply.github.com>
2023-12-06 11:38:26 +0100

    menu refonctionne :)

--- AHaChaSha/Assets/UI Implementation/MenuAssets/MainMenu.cs
+++ AHaChaSha/Assets/UI Implementation/MenuAssets/MainMenu.cs
@@ -2,18 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public AudioSource backgroundMusic; // Reference to your background music AudioSource
+    public Slider musicSlider; // Reference to the UI Slider
+
+    private void Start()
+    {
+        // Set the slider's value to the current music volume
+        if (backgroundMusic != null && musicSlider != null)
+        {
+            musicSlider.value = backgroundMusic.volume;
+        }
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        Debug.Log("UBZDYIVGUHDAIBZCJVQDHGYIHUCJLNS");
+    }
+
+    public void OptionGame()
+    {
+        // Handle options menu (if needed)
     }
 
     public void QuitGame()
     {
-        Debug.Log("quitter");
-        
+        Application.Quit();
+    }
+
+    // Method to adjust the background music volume based on the slider value
+    public void AdjustMusicVolume()
+    {
+        if (backgroundMusic != null && musicSlider != null)
+        {
+            backgroundMusic.volume = musicSlider.value;
+        }
     }
 }
