a66ef187e11815717fdb652900ca42459fe4c306
Sasha D <97671299+Sashadjm@users.noreply.github.com>
2023-12-07 02:02:14 +0100

    suppression

--- AHaChaSha/Assets/MenuAssets/MainMenu.cs
+++ AHaChaSha/Assets/MenuAssets/MainMenu.cs
@@ -6,26 +6,10 @@ using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    public AudioSource backgroundMusic; // Reference to your background music AudioSource
-    public Slider musicSlider; // Reference to the UI Slider
-
-    private void Start()
-    {
-        // Set the slider's value to the current music volume
-        if (backgroundMusic != null && musicSlider != null)
-        {
-            musicSlider.value = backgroundMusic.volume;
-        }
-    }
 
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }
-
-    public void OptionGame()
-    {
-        // Handle options menu (if needed)
     }
 
     public void QuitGame()
@@ -33,12 +17,4 @@ public class MainMenu : MonoBehaviour
         Application.Quit();
     }
 
-    // Method to adjust the background music volume based on the slider value
-    public void AdjustMusicVolume()
-    {
-        if (backgroundMusic != null && musicSlider != null)
-        {
-            backgroundMusic.volume = musicSlider.value;
-        }
-    }
 }
